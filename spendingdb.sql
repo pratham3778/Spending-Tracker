38534909f28d783790e57d1e4226ea1df3102110
Pratham Tanpure <140080210+pratham3778@users.noreply.github.com>
2024-11-21 19:30:27 +0530

    Create spendingdb.sql

---
+++ spendingdb.sql
@@ -0,0 +1,8 @@
+-- create schema
+-- tables -> create table -> category_info and spendings
+
+-- category_info
+SELECT * FROM spendingdb.category_info;
+
+-- spendings
+SELECT * FROM spendingdb.spendings;
